055451ca90683de7e9a31f05d9d20577a726666e
Mrid22 <mridulaga@outlook.com>
2026-04-21 16:22:34 +0300

    initialize

--- flake.nix
+++ flake.nix
@@ -5,11 +5,41 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShells."${system}".default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        cargo
+        rustc
+        rustfmt
+        clippy
+        rust-analyzer
+        glib
+      ];
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+      ];
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    };
 
+    packages."${system}".default = pkgs.rustPlatform.buildRustPackage {
+      name = "taksnotes";
+      src = ./.;
+      buildInputs = with pkgs; [
+        glib
+      ];
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+      ];
+
+      cargoLock.lockFile = ./Cargo.lock;
+    };
   };
 }
